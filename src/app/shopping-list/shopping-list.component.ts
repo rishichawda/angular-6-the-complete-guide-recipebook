6b3f8f0c3c9b20c2d3599bfaa93ca7a046c42b15
Rishi Kumar <chawda.rishii@gmail.com>
2018-05-22 12:39:01 +0530

    updt slserve

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,22 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
+  providers: [ShoppingListService]
 })
 export class ShoppingListComponent implements OnInit {
 
-  ingredients: Ingredient[] = [
-    new Ingredient('Apple', 5),
-    new Ingredient('Tomato', 10)
-  ];
+  // ingredients: Ingredient[] = [
+  //   new Ingredient('Apple', 5),
+  //   new Ingredient('Tomato', 10)
+  // ];
 
+  ingredients: Ingredient[];
 
-  constructor() { }
+  constructor(private shoppinglist_service: ShoppingListService) { }
 
   ngOnInit() {
+    this.ingredients = this.shoppinglist_service.get_ingredients();
   }
 
   add_to_list(new_item){
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,12 @@
+import { Ingredient } from "../shared/ingredient.model";
+
 export class ShoppingListService {
-    
+    private ingredients: Ingredient[] = [
+        new Ingredient('Apple', 5),
+        new Ingredient('Tomato', 10)
+      ];
+
+    get_ingredients() {
+        return this.ingredients.slice();
+    }
 }
