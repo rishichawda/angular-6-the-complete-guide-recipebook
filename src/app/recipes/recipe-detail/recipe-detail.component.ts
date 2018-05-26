abd52ed01a79e9bed7595030db505d12b497405b
Rishi Kumar <chawda.rishii@gmail.com>
2018-05-26 19:21:41 +0530

    link recipe and shopping list

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../recipes.model';
+import { ShoppingListService } from '../../shopping-list/shopping-list.service';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
-  styleUrls: ['./recipe-detail.component.css']
+  styleUrls: ['./recipe-detail.component.css'],
 })
 export class RecipeDetailComponent implements OnInit {
 
   @Input('recipe_detail') recipe: Recipe;
 
-  constructor() { }
+  constructor(private shoppinglist_service: ShoppingListService) { }
 
   ngOnInit() {
+  }
+
+  send_to_shoplist() {
+    for (var i in this.recipe.ingredients) {
+      this.shoppinglist_service.add_ingredients(this.recipe.ingredients[i]);
+    }
   }
 
 }
