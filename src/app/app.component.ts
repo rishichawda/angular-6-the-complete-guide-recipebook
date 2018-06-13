040750100436002b1584795ecd0ab3baa1c47854
Rishi Kumar <chawda.rishii@gmail.com>
2018-06-13 12:34:02 +0530

    minor fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  show_section = 'recipe-book';
-
-  change_section(section_name) {
-    this.show_section = section_name;
-  }
-
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,19 +8,4 @@ import { Router } from '@angular/router';
 })
 
 export class HeaderComponent {
-    @Output('on_section_select') selection = new EventEmitter<string>();
-    active_tab = 'recipe-book';
-
-    constructor(private router: Router) {}
-
-    on_select_section(section_name: string) {
-        this.active_tab = section_name;
-        if(this.active_tab === 'recipe-book') {
-            this.router.navigate(['recipes']);
-        } else {
-            this.router.navigate(['shopping']);
-        }
-        this.selection.emit(this.active_tab);
-    }
-
 }
